639af6aaae74b62945fc8865f60c6238ae8b5569
rafidreezwan <69665294+rafidreezwan@users.noreply.github.com>
2021-12-04 01:52:41 +0600

    Add files via upload

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << a<<"\n";
+    cout << b<<"\n";
+    cout << c<<"\n";
+    cout << d<<"\n";
+    cout << e<<"\n";
+
+
+    return 0;
+}
